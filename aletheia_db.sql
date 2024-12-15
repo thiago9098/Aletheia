b69db3e37aec7a4672bc73cb65b452f37e91765a
JorgeKalluf <117755526+JorgeKalluf@users.noreply.github.com>
2024-12-15 20:19:31 -0300

    Add files via upload

---
+++ aletheia_db.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE  IF NOT EXISTS `aletheia_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `aletheia_db`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: aletheia_db
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES ('sepae.pinhais@exemplo.com','$2y$10$HQ3sg42jefT04NOkjQFTd.PAw1xpUaniZKtA864BGSocDmfa1iKxu');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno` (
+  `nome` varchar(255) DEFAULT 'Anônimo',
+  `senha` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `CPF` varchar(255) NOT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `CPF` (`CPF`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno`
+--
+
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+INSERT INTO `aluno` VALUES ('Anônimo','carlos123','carlos.pereira@email.com','12345678909'),('usuarioteste','$2y$10$Pk1Qw13QOIfW0vM/y9WfwOu5.X7/TdXL/D7XbmhGRPJvgK1NmTJiK','exemplo@gmail.com','123123123'),('João Silva','joao123','joao.silva@email.com','11144477735'),('jorge kalluf','$2y$10$tGXR8hljEApvmALo8ZPlQes9ZHam9YuB/wRGRpa4D6nzeGu/ksDIG','jorge@gmail.com','1234596128'),('Maria Oliveira','maria123','maria.oliveira@email.com','22255588846');
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comentario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `texto` text NOT NULL,
+  `data` datetime NOT NULL,
+  `aluno_email` varchar(255) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `aluno_email` (`aluno_email`),
+  CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`aluno_email`) REFERENCES `aluno` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentario`
+--
+
+LOCK TABLES `comentario` WRITE;
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+INSERT INTO `comentario` VALUES (1,'A iluminação das salas está muito fraca, prejudicando a leitura e os estudos.','2024-12-03 10:00:00','joao.silva@email.com','Problemas com iluminação'),(2,'As cadeiras do laboratório de informática estão quebradas e desconfortáveis.','2024-12-03 11:00:00','maria.oliveira@email.com','Cadeiras quebradas no laboratório'),(4,'A biblioteca precisa de mais exemplares de livros atualizados para os cursos técnicos.','2024-12-03 12:00:00','carlos.pereira@email.com','Biblioteca desatualizada');
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'aletheia_db'
+--
+
+--
+-- Dumping routines for database 'aletheia_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-15 20:18:43
